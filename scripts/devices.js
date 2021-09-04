66ddc98b82c4144983c0466a753b2bfc7b8372e0
Mauricio santos <mauriciodssbitcoin@gmail.com>
2021-09-03 21:40:32 -0300

    first commit

---
+++ scripts/devices.js
@@ -0,0 +1,8 @@
+function menu_active (){
+    return document.querySelector('.menu-mobile').classList.add('active')
+    
+}
+
+function close_menu(){
+    return document.querySelector('.menu-mobile').classList.remove('active')
+}
